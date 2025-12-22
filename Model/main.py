3aa334c7dae87d88331ccaa7bb0e3fa81ef336a2
H.M.B.S Nethranjana Adhikari <binaranethranganaadikari@gmail.com>
2025-12-22 23:21:46 +0530

    import _the_data

---
+++ Model/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+
+
+def get_clean_data():
+    data = pd.read_csv("Data/data.csv")
+    print(data.head())
+    return data
+
+
+
+def main():
+    data = get_clean_data()
+
+if __name__ == "__main__":
+    main()
